7590e0be826c5e9e956f7c3a0359e566d9d950ea
Young <96588971+zer0kola@users.noreply.github.com>
2022-05-01 21:20:01 +0900

    [Bronze III] Title: 별 찍기 - 2, Time: 364 ms, Memory: 19008 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            for (int j =num; j>i+1; j--){
+                System.out.print(' ');
+            }for (int k = 0; k <= i; k++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
