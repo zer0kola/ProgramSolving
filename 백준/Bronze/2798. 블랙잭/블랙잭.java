9ac3564073acd449752d49633af99abde175b667
정영호 <96588971+zer0kola@users.noreply.github.com>
2022-05-16 19:51:38 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -7,25 +7,25 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int max=-1;
-        int temp=-1;
         int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        System.out.println(search(arr,n,m));
+    }
+
+    static int search(int[] arr, int n, int m) {
+        int max = 0;
         for (int i = 0; i < n-2; i++) {
             for (int j = i+1; j < n-1; j++) {
                 for (int k = j+1; k < n; k++) {
-                    temp = arr[i] + arr[j] + arr[k];
-                    if (temp == m) break;
+                    int temp = arr[i] + arr[j] + arr[k];
+                    if (temp == m) return temp;
                     else if (temp > max && temp <m) max = temp;
                 }
-                if (temp == m) break;// break를 하나만 쓰면 for 문 하나만 나옴
             }
-            if (temp == m) break;
         }
-        if (temp==m) System.out.println(temp);
-        else System.out.println(max);
+        return max;
     }
 }
