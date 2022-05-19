57302c2e3b38c4f67a020bf392bbc8ade46f1264
zer0kola <zer0kola321@gmail.com>
2022-05-20 01:37:07 +0900

    0520

--- src/Boj11654.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Boj11654 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        char ch = scanner.next().charAt(0);
-        System.out.println((int)ch);
-    }
-}
