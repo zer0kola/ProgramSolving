270f76b08a72a12310953eab0addb4ce2b2edbb9
정영호 <96588971+zer0kola@users.noreply.github.com>
2022-05-23 19:56:04 +0900

    [Silver V] Title: 소인수분해, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i <= Math.sqrt(N); i++) { // 제곱근까지만
+            while (N % i == 0) { // 2부터 시작해서 안 나눠질때까지지
+                sb.append(i).append('\n');
+                N /= i;
+            }
+        }
+        if (N != 1) {
+            sb.append(N);
+        }
+        System.out.println(sb);
+    }
+}
