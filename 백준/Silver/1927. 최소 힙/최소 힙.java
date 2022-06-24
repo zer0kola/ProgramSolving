7ee379fe51ad009354ce341024e167cb650ab0d7
zer0kola <zer0kola321@gmail.com>
2022-06-24 23:13:40 +0900

    0624

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -5,15 +5,14 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        PriorityQueue<Integer> pQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
             if (num == 0){
-                if(pQueue.isEmpty()) System.out.println('0');
-                else System.out.println(pQueue.poll());
-            } else pQueue.add(num);
+                if(minHeap.isEmpty()) System.out.println('0');
+                else System.out.println(minHeap.poll());
+            } else minHeap.add(num);
         }
     }
 }
