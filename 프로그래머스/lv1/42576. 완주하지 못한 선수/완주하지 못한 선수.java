2a3e76c42624c6f2e84de8ce19205d2b61f33445
정영호 <96588971+zer0kola@users.noreply.github.com>
2022-06-30 06:21:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 71.00 ms, Memory: 95.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution
+{
+public String solution(String[] participant, String[] completion)
+    {
+        String answer = "";
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String name : participant)
+        {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+        
+        for (String name : completion)
+        {
+            map.put(name, map.get(name) - 1);
+        }
+        
+        for (String key : map.keySet())
+        {
+            // 값이 하나 이상 있을 경우
+            if (map.get(key) > 0)
+            {
+                answer = key;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
